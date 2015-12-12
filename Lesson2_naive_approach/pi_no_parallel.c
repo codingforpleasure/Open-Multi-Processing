116a49f6719598b68ed509ebd910eff6569e6195
Gilc <Cohengil333@gmail.com>
2015-12-13 00:21:46 +0200

    Introduction to the question we are facing

--- Lesson2_naive_approach/pi_no_parallel.c
+++ Lesson2_naive_approach/pi_no_parallel.c
@@ -3,14 +3,17 @@
 
 static long num_steps = 10000000;
 double step;
-void main()
+
+int main(int argc, const char *argv[])
 {
 	int i;
 	double x,pi, sum = 0.0;
 	step = 1.0 / (double) num_steps;
 	double time_before, time_after,interval;
 
+	/* Taking timestamp before calculation process started */
 	time_before = omp_get_wtime();
+
 	for (i=0 ; i < num_steps; i++)
 	{
 		x = (i+0.5)*step;
@@ -19,8 +22,13 @@ void main()
 
 	pi = step * sum;
 
-	printf("\nThe value of pie is: %.5f\n", pi);
+	printf("\nThe value of pi is: %.5f\n", pi);
+
+	/* Taking timestamp after calculation process ended */
 	time_after = omp_get_wtime();
+
 	interval = time_after - time_before;
-	printf("\nThe interval time took: %f\n", interval);
+	printf("\nThe duration time it took to calculate pi was: %f seconds\n", interval);
+	return 0;
 }
+
